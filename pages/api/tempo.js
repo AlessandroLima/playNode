192f5d8075d81cdaf7d396bfdd0a1c0449b7c981
Alessandro Teixeira Lima <alessandro.lima-terceiro@cea.com.br>
2021-01-21 07:11:13 -0300

    exibindo temperatura

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,13 @@
-function tempo(request, response) {
-    const dinamicDate = new Date();
+//
+
+async function tempo(request, response) {
+
+    const weatherResponse = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=3e40fae3f25784f4776d736951958715");
+    const weatherResponseJson = await weatherResponse.json();
+    const temperature = weatherResponseJson.main.temp;
     response.json(
         {
-            date: dinamicDate.toGMTString()
+            temperature: temperature
         }
     );
 }
