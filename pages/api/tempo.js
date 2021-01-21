38177ed7f8ed72ecde0aac95f0920fe992d6fb2d
Alessandro Teixeira Lima <alessandro.lima-terceiro@cea.com.br>
2021-01-21 10:15:49 -0300

    add apiKey

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,13 +1,16 @@
 //
 
 async function tempo(request, response) {
-
-    const weatherResponse = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=3e40fae3f25784f4776d736951958715");
+    
+    const apiKey = process.env.API_KEY;
+    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=${apiKey}`);
     const weatherResponseJson = await weatherResponse.json();
     const temperature = weatherResponseJson.main.temp;
+    const city = weatherResponseJson.name;
     response.json(
         {
-            temperature: temperature
+            temperature: temperature,
+            city: city
         }
     );
 }
