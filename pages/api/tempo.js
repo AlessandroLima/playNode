2b8d73e21af6b758857d49d58c58a0ea4735347b
aletlima <aletlima@gmail.com>
2021-02-27 16:32:26 -0300

    Fix Temperature

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,27 +1,27 @@
-async function tempo(request, response) {
+// async function tempo(request, response) {
     
-    const apiKey = process.env.API_KEY;
-    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=${apiKey}`);
-    const weatherResponseJson = await weatherResponse.json();
-    const temperature = weatherResponseJson.main.temp;
-    const city = weatherResponseJson.name;
-    const temp = new Date();
+//     const apiKey = process.env.API_KEY;
+//     const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=${apiKey}`);
+//     const weatherResponseJson = await weatherResponse.json();
+//     const temperature = weatherResponseJson.main.temp;
+//     const city = weatherResponseJson.name;
+//     const temp = new Date();
     
-    response.setHeader('Cache-Control', 's-maxage=10', 'stale-while-revalidate');
-    response.json(
-        {
-            temperature: temperature,
-            city: city,
-            hour: temp
-        }
-    );
+//     //response.setHeader('Cache-Control', 's-maxage=10', 'stale-while-revalidate');
+//     response.json(
+//         {
+//             temperature: temperature,
+//             city: city,
+//             hour: temp
+//         }
+//     );
 
-    return {
-        results: {
-            weatherResponseJson,
-        },
-      }
-}
+//     return {
+//         results: {
+//             weatherResponseJson,
+//         },
+//       }
+// }
 
 function RenderPageTempo() {
     return (
