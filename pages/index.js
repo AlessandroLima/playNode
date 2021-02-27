424b594b8b2df58c3b3ae33c5e6f177ddaa51ed1
aletlima <aletlima@gmail.com>
2021-02-27 16:42:09 -0300

    Fix Temperature

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,3 @@ function Home({tempo}) {
 
 export default Home
 
-function Blog({ posts }) {
-    return (
-      <ul>
-        {posts.map((post) => (
-          <li>{post.title}</li>
-        ))}
-      </ul>
-    )
-  }
-  
-  export default Blog
