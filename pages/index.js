512643e460e482f612dca2afed4b2cce163451e6
aletlima <aletlima@gmail.com>
2021-02-27 17:05:48 -0300

    Fix Temperature

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 // posts will be populated at build time by getStaticProps()
-function Temperatura({ weatherResponseJson }) {
+function Blog({ posts }) {
     return (
       <ul>
-        {weatherResponseJson.map((temp) => (
-          <li>{temp.name}</li>
+        {posts.map((post) => (
+          <li>{post.name}</li>
         ))}
       </ul>
     )
@@ -12,23 +12,20 @@ function Temperatura({ weatherResponseJson }) {
   // This function gets called at build time on server-side.
   // It won't be called on client-side, so you can even do
   // direct database queries. See the "Technical details" section.
-  export async function tempo() {
+  export async function getStaticProps() {
     // Call an external API endpoint to get posts.
     // You can use any data fetching library
     const apiKey = process.env.API_KEY;
-    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=${apiKey}`);
-    const weatherResponseJson = await weatherResponse.json();
-    const temperature = weatherResponseJson.main.temp;
-    const city = weatherResponseJson.name;
-    
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Osasco&units=metric&appid=${apiKey}`);
+    const posts = await res.json()
   
     // By returning { props: posts }, the Blog component
     // will receive `posts` as a prop at build time
     return {
       props: {
-        weatherResponseJson,
+        posts,
       },
     }
   }
   
-  export default Temperatura
+  export default Blog
